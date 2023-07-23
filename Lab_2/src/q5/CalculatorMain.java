0ec19d966bb4d3c4c4d6490d0973c0a6dbd05715
Nazal-nihad <nazalnihad17@gmail.com>
2023-07-24 02:38:23 +0530

    java gui calculator without drag and drop

---
+++ Lab_2/src/q5/CalculatorMain.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package q5;
+
+/**
+ *
+ * @author nazal
+ */
+public class CalculatorMain {
+    public static void main(String[] args){
+    Calculator calculator = new Calculator();
+//    calculator();
+    }
+}
